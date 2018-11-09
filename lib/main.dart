f1f167b3ee71deed22027857fecbce61370669ef
JuliaVaca <thesimpatic@hotmail.com>
2018-11-09 12:32:02 -0600

    primer commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+void main(){
+ runApp(new Application());
+}
+class Application extends StatelessWidget {
+ @override
+ Widget build(BuildContext context) {
+ return new MaterialApp(
+ title: "Stateless Widget",
+ home: new Scaffold(
+ body: new Container(
+ color: Colors.pink,
+ child: new Container(
+ color: Colors.yellow,
+ margin: const EdgeInsets.all(30.0),
+ ),
+ )
+ 
+ ),
+ );
+ }
+}
